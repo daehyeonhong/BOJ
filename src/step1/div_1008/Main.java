cf9551304eae9965404b56b06fd132a329681351
HongDae <fholng@outlook.com>
2021-12-10 12:51:26 +0900

    [Algorithm] Step.One
    div

---
+++ src/step1/div_1008/Main.java
@@ -0,0 +1,16 @@
+package step1.div_1008;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextInt();
+        double b = scanner.nextInt();
+
+        System.out.print(a / b);
+    }
+
+}
