0841b2decf20aed521c2b4a9ab053d6de2ded07e
HongDae <fholng@outlook.com>
2021-12-10 12:51:17 +0900

    [Algorithm] Step.One
    arithmetic

---
+++ src/step1/arithmeticOperation_10869/Main.java
@@ -0,0 +1,20 @@
+package step1.arithmeticOperation_10869;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+
+}
