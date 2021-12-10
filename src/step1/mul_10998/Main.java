c0c94adf02bf5d46e72e13d0c88981c1791caa00
HongDae <fholng@outlook.com>
2021-12-10 12:51:33 +0900

    [Algorithm] Step.One
    mul

---
+++ src/step1/mul_10998/Main.java
@@ -0,0 +1,16 @@
+package step1.mul_10998;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.print(a * b);
+    }
+
+}
