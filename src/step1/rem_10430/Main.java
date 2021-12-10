d5a273f88d7ff21aa406ed9f22d63eed1674cedd
HongDae <fholng@outlook.com>
2021-12-10 12:51:38 +0900

    [Algorithm] Step.One
    rem

---
+++ src/step1/rem_10430/Main.java
@@ -0,0 +1,20 @@
+package step1.rem_10430;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+
+}
